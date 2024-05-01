8ef69da8bb87b7501f0d3d477b91bcc3ca0d3503
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-01 20:51:12 -0300

    Create ConstantsValues class in data folder

---
+++ lib/src/core/data/constants_values.dart
@@ -0,0 +1,6 @@
+class ConstantsValues {
+  static double movieCardHeight = 275;
+  static double moviesWrapListRunSpacing = 10;
+  static double movieCardBorderRadius = 5;
+  static double movieCardMaxWidth = 160;
+}
