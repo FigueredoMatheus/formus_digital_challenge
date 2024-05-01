1b48e3183658a2223ad4fa1c4d84c63d6cd5ffc2
Matheus Figueredo <matheusfigueredob@gmail.com>
2024-05-01 18:53:41 -0300

    Create home page bloc structure with Equatable package

--- lib/src/modules/home/bloc/home_page_event.dart
+++ lib/src/modules/home/bloc/home_page_event.dart
@@ -1,4 +1,8 @@
 part of 'home_page_bloc.dart';
 
-@immutable
-sealed class HomePageEvent {}
+sealed class HomePageEvent extends Equatable {
+  const HomePageEvent();
+
+  @override
+  List<Object> get props => [];
+}
--- lib/src/modules/home/bloc/home_page_state.dart
+++ lib/src/modules/home/bloc/home_page_state.dart
@@ -1,6 +1,16 @@
 part of 'home_page_bloc.dart';
 
-@immutable
-sealed class HomePageState {}
+sealed class HomePageState extends Equatable {
+  const HomePageState();
+
+  @override
+  List<Object> get props => [];
+}
 
 final class HomePageInitial extends HomePageState {}
+
+final class LoadingState extends HomePageState {}
+
+final class FailOnFetchMoviesState extends HomePageState {}
+
+final class SuccessOnFetchMoviesState extends HomePageState {}
